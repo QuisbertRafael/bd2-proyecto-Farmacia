187963d379cacfb3683a7b16df025afce2ba8d13
Mateo Riveros <mateitorr2005@gmail.com>
2026-05-24 22:57:49 -0400

    US-03 agregar endpoint para crear productos

--- src/routes/product.py
+++ src/routes/product.py
@@ -1,8 +1,22 @@
 from flask import Blueprint
-from services.product import get_products_service
+from services.product import (
+    get_products_service,
+    create_product_service,
+    new_product_form_service
+)
 
 product = Blueprint('product', __name__)
 
 @product.route('/', methods=['GET'])
 def get_products():
     return get_products_service()
+
+# Mostrar formulario
+@product.route('/new', methods=['GET'])
+def new_product():
+    return new_product_form_service()
+
+# Guardar producto
+@product.route('/new', methods=['POST'])
+def create_product():
+    return create_product_service()
--- src/services/product.py
+++ src/services/product.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from config.mongodb import mongo
 
 def get_products_service():
@@ -10,3 +10,24 @@ def get_products_service():
         'products.html',
         products=products
     )
+
+# Mostrar formulario
+def new_product_form_service():
+    return render_template('new_product.html')
+
+
+# Crear producto
+def create_product_service():
+
+    product = {
+        'nombre': request.form.get('nombre'),
+        'descripcion': request.form.get('descripcion'),
+        'precio': float(request.form.get('precio')),
+        'stock': int(request.form.get('stock')),
+        'categoria': request.form.get('categoria'),
+        'imagen': request.form.get('imagen')
+    }
+
+    mongo.db.productos.insert_one(product)
+
+    return redirect('/products')
